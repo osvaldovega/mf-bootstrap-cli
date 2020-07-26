01033cd1c8877b2aca86c390b5a0cf31aefda7e8
Osvaldo Vega <osvaldo.vega@raisin.com>
2020-07-26 23:17:07 +0200

    Refactor(TS Template): Update TS files changing file ext from jsx to tsx

--- templates/typescript/src/bootstrap.tsx
+++ templates/typescript/src/bootstrap.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const root = document.createElement("root");
+const root = document.createElement('root');
 ReactDOM.render(<App />, root);
 document.body.appendChild(root);
