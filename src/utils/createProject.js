efc679ec93fe4a13a20895e6d257b950f3d138b8
Osvaldo Vega <osvaldo.vega@raisin.com>
2020-07-26 23:18:18 +0200

    Refactor(createProject): Update logic of the steps to create a new file

--- src/utils/createProject.js
+++ src/utils/createProject.js
@@ -1,69 +1,106 @@
 import fs from 'fs';
 import path from 'path';
-import chalk from 'chalk';
 import ncp from 'ncp';
 import { promisify } from 'util';
 import execa from 'execa';
 import Listr from 'listr';
 import { projectInstall } from 'pkg-install';
+import { mainBannerMessage, SuccessMessage } from './logMessage';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
 
-const copyTemplateFiles = async (options) => {
-	return copy(options.templateDirectory, options.targetDirectory, {
-		clobber: false,
-	});
-};
-
-const initGit = async (options) => {
-	const result = await execa('git', ['init'], {
-		cwd: options.targetDirectory,
-	});
-
-	if (result.failed) return Promise.reject(new Error('Failed to initialize Git'));
-
+const checkAccessToTemplateDir = async ({ templateDirectory }) => {
+	try {
+		await access(templateDirectory, fs.constants.R_OK);
+	} catch (err) {
+		return Promise.reject(new Error('Missing template files, the select template it is not valid'));
+	}
 	return;
 };
 
-export const createProject = async (options) => {
-	options = {
-		...options,
-		targetDirectory: options.targetDirectory || process.cwd(),
-	};
-
-	const currentFileUrl = import.meta.url;
-
-	const templateDir = path.resolve(new URL(currentFileUrl).pathname, '../../templates', options.template.toLowerCase());
-
-	options.templateDirectory = templateDir;
-
+const createNewProjectDir = async ({ appName, targetDirectory }) => {
 	try {
-		await access(templateDir, fs.constants.R_OK);
+		await execa('mkdir', [appName], { cwd: targetDirectory });
 	} catch (err) {
-		console.error('%s Invalid template name', chalk.red.bold('ERROR'));
-		process.exit(1);
+		return Promise.reject(
+			new Error(`Failed to create directory ${appName}, check there is no other directory with the same name`),
+		);
 	}
+	return;
+};
 
+const copyTemplateFiles = async ({ templateDirectory, targetDirectory, appName }) => {
+	try {
+		await copy(templateDirectory, path.join(targetDirectory, appName), { clobber: false });
+	} catch (error) {
+		return Promise.reject(new Error('Failed to copy files to destiny directory'));
+	}
+	return;
+};
+
+const initGit = async ({ targetDirectory, appName }) => {
+	try {
+		await execa('git', ['init'], { cwd: path.join(targetDirectory, appName) });
+	} catch (error) {
+		return Promise.reject(new Error('Failed to initialize Git'));
+	}
+	return;
+};
+
+const installProjectDependencies = async ({ targetDirectory, appName, packageManager }) => {
+	try {
+		await projectInstall({ cwd: path.join(targetDirectory, appName), prefer: packageManager });
+	} catch (error) {
+		return Promise.reject(new Error('Error installing project dependencies.'));
+	}
+	return;
+};
+
+const runTasks = async (options) => {
 	const tasks = new Listr([
 		{
-			title: 'Copy project files.',
+			title: 'Checking Project Files.',
+			task: () => checkAccessToTemplateDir(options),
+		},
+		{
+			title: 'Creating Project Directory.',
+			task: () => createNewProjectDir(options),
+		},
+		{
+			title: 'Coping project files.',
 			task: () => copyTemplateFiles(options),
 		},
 		{
-			title: 'Initialize Git.',
+			title: 'Initializing Git.',
 			task: () => initGit(options),
-			enabled: () => options.git,
+			skip: () => !options.git,
 		},
 		{
-			title: 'Install dependecies.',
-			task: () => projectInstall({ cwd: options.targetDirectory }),
-			skip: () => (!options.runInstall ? 'Pass --install to automatically install dependcies' : undefined),
+			title: 'Installing dependencies.',
+			task: () => installProjectDependencies(options),
+			skip: () => !options.install,
 		},
 	]);
 
 	await tasks.run();
+};
 
-	console.log('%s Project ready', chalk.green.bold('DONE'));
+export const createProject = async (options) => {
+	const newOptions = {
+		...options,
+		targetDirectory: options.targetDirectory || process.cwd(),
+		templateDirectory: path.resolve(__dirname, '../../templates', options.template.toLowerCase()),
+	};
+
+	try {
+		mainBannerMessage();
+		await runTasks(newOptions);
+		SuccessMessage();
+	} catch (err) {
+		console.log('\n');
+		process.exit(1);
+	}
+
 	return true;
 };
