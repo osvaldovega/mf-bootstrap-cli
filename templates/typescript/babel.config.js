504e0d19b42a552b8e6856fc6bf7f990be464aea
Osvaldo Vega <osvaldo.vega@raisin.com>
2020-08-28 00:06:54 +0200

    Refactor(templates/TS): Update all files after run eslint and add husky

--- templates/typescript/babel.config.js
+++ templates/typescript/babel.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-	presets: [
-		['@babel/preset-env', { modules: false }],
-		'@babel/preset-typescript',
-		'@babel/preset-react',
-	],
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
 
-	plugins: [
-		'react-hot-loader/babel',
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-syntax-dynamic-import',
-	],
+  plugins: [
+    'react-hot-loader/babel',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
 
-	env: {
-		test: {
-			plugins: ['@babel/plugin-transform-modules-commonjs', 'dynamic-import-node'],
-		},
-	},
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs', 'dynamic-import-node'],
+    },
+  },
 };
